6a4e082aa39a1f25a31ab6c3e12368ffb072db0a
Mahdi-HSh <mahdi.hoseini1381@yahoo.com>
2024-05-29 17:12:40 +0330

    fixed a bug in offline mode exits list wasn't updating correctly. used init function for env properties. also an unnecessary function removed.

--- env.py
+++ env.py
@@ -5,19 +5,19 @@ glenda_ai = glendaAI.bfs()
 
 class GlendyEnv():
 
-  rows = 11
-  columns = 11
-  start = (5, 5)
-  blocks = []
-  base_exits = [
-         (0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (0, 7), (0, 8), (0, 9), (0, 10),
-         (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0), (8, 0), (9, 0), (10, 0),
-         (10, 1), (10, 2), (10, 3), (10, 4), (10, 5), (10, 6), (10, 7), (10, 8), (10, 9), (10, 10),
-         (1, 10), (2, 10), (3, 10), (4, 10), (5, 10), (6, 10), (7, 10), (8, 10), (9, 10)
-               ]
-  exits = base_exits.copy()
-  actions = ['ne', 'e', 'se', 'nw', 'w', 'sw']
-  result = ""
+  def __init__(self):
+    self.rows = 11
+    self.columns = 11
+    self.start = (5, 5)
+    self.blocks = []
+    self.exits = [
+          (0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (0, 7), (0, 8), (0, 9), (0, 10),
+          (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0), (8, 0), (9, 0), (10, 0),
+          (10, 1), (10, 2), (10, 3), (10, 4), (10, 5), (10, 6), (10, 7), (10, 8), (10, 9), (10, 10),
+          (1, 10), (2, 10), (3, 10), (4, 10), (5, 10), (6, 10), (7, 10), (8, 10), (9, 10)
+          ]
+    self.actions = ['ne', 'e', 'se', 'nw', 'w', 'sw']
+    self.result = ""
 
   def arrange_blocks(self, difficulty):
     self.blocks.clear()
@@ -38,16 +38,6 @@ class GlendyEnv():
     for block in self.blocks:
       if block in self.exits:
         self.exits.remove(block)
-
-  def reset_state(self): # extra
-    self.exits = self.base_exits.copy()
-    self.blocks.clear()
-    self.arrange_blocks()
-    state = [[0 for i in range(11)] for i in range(11)]
-    for block in self.blocks:
-      state[block[0]][block[1]] = -1
-    state[self.start[0]][self.start[1]] = 1
-    return state
   
   def glenda_around(self, glenda):
     if glenda[0] % 2 == 0:
